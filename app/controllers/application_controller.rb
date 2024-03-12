f0638bba089bd4c6ff023484ca00ee5f4df408a1
Ines Alvergne <i.alvergne@gmail.com>
2024-03-12 22:07:05 +0100

    style navbar for desktop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def set_user
     @ines = User.first
   end
+
+  def display_background_green_gradient
+    @display_background_green_gradient = true
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_user, only: [:home]
+  before_action :display_background_green_gradient, only: [:home]
 
   def home
     @tagline = @ines.tagline
