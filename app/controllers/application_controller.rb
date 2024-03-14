abe0fc62f27db244a638bef53ecfa35e7d05a437
Ines Alvergne <i.alvergne@gmail.com>
2024-03-14 21:06:51 +0100

    add method to verify that user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def display_background_green_gradient
     @display_background_green_gradient = true
   end
+
+  def verify_admin
+    redirect_to root_path and return unless current_user&.admin?
+  end
 end
