041642a8b607a6b63a4c821b470164604e09fbac
Ines Alvergne <i.alvergne@gmail.com>
2024-08-29 00:48:07 +0200

    add rough notation and play around with animations on the cards

---
+++ app/javascript/controllers/annotate_title_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import { annotate } from "rough-notation";
+
+export default class extends Controller {
+
+  connect() {
+    const options = { type: "underline",
+                      color: "#009486",
+                      strokeWidth: "2",
+                      padding: 15 };
+
+    const annotation = annotate(this.element, options);
+    annotation.show();
+  }
+}
