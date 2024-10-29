57a58b023dc84a9778a0d916f9c91fa371841b84
Ines Alvergne <i.alvergne@gmail.com>
2024-10-29 22:12:00 +0100

    header container green and navbar hover border-top

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   before_action :display_background_green_gradient, only: [:home]
 
   def home
+    @navbar_light = true
+
     @tagline = @me.tagline
     @available = @me.available
   end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,8 @@
 class ServicesController < ApplicationController
+
   def index
+    @current_view = "services"
+
     @services = Service.all
   end
 end
